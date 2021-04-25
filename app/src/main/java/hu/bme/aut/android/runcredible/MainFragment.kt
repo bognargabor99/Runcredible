8675f59e1006fcde0bae338727513f41cb1b8413
bognargabor99 <gaborbognar99@gmail.com>
2021-04-25 11:59:21 +0200

    Navigation component and MainFragment layout

---
+++ app/src/main/java/hu/bme/aut/android/runcredible/MainFragment.kt
@@ -0,0 +1,21 @@
+package hu.bme.aut.android.runcredible
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import hu.bme.aut.android.runcredible.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+    private lateinit var binding: FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentMainBinding.inflate(LayoutInflater.from(requireContext()))
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+}
